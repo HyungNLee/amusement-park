1797c1430495e5d8958ae2314674039b6e7a1a3c
Derek Smith and Hyung Lee <hyung@>
2018-08-09 10:38:52 -0700

    change layout to highlight instead.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,19 @@ var height = parseFloat(prompt("Enter a height in inches:"));
 
 if(height < 36){
 	alert("You are too short to ride");
+  $(".shortHeightRide").addClass("highlight");
 } else if(height < 90 && height >= 36 ){
   alert("You are tall enough for all rides.");
-  $(".mediumRides").show();
+  $(".shortHeightRide").addClass("highlight");
+  $(".mediumHeightRide").addClass("highlight");
 }
 else if (height >=90)
 {
   alert("Too tall to ride.")
-  $(".tallRides").show();
+  $(".shortHeightRide").addClass("highlight");
+  $(".mediumHeightRide").addClass("highlight");
+  $(".tallHeightRide").addClass("highlight");
+
 
 }
 })
